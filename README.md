ce2a96c8997b33638d7870f5d46ae20089cd0dfd
anthonymiloradovic <anthony.miloradovic@icloud.com>
2023-02-01 01:21:39 +0100

    add readme

---
+++ README.md
@@ -0,0 +1,10 @@
+## Bloc-note en react
+
+<br>
+- Mission : Faire un blocs-note<br><br>
+<p>Projet en construction
+</p>
+<img src="./assets/screen1.png" width="700px"> <br><br><br>
+
+
+<p> Ciao 	&#129302;</p>
--- src/ButtonDelete.jsx
+++ src/ButtonDelete.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import './ButtonDelete.css';
 
 const ButtonDelete = ({ index, handleDelete }) => (
-    <button onClick={() => handleDelete(index)}> x</button>
+    <button className='btn-delete' onClick={() => handleDelete(index)}> x</button>
   );
 export default ButtonDelete;
